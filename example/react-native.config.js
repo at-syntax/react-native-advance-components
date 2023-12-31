05ad801ddacaba25b22fd63ab97ee7fc89e7df32
Saikat Samanta <saikatsamanta737@gmail.com>
2024-01-01 00:32:53 +0530

    refactor: :bug: update deps

--- example/react-native.config.js
+++ example/react-native.config.js
@@ -3,6 +3,11 @@ const pak = require('../package.json');
 
 module.exports = {
   dependencies: {
+    'react-native-vector-icons': {
+      platforms: {
+        ios: null,
+      },
+    },
     [pak.name]: {
       root: path.join(__dirname, '..'),
     },
